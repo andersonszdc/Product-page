e41f80d8c5fddf7d5dd6000c9e00f900337bdc4b
Anderson Souza <anderson.souza@impar.com.br>
2022-02-14 14:59:43 -0300

    created classes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,43 @@
-import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import "../styles/index.module.css";
 
-export default function Home() {
+export default function Index() {
   return (
     <main>
-      <div>
-        <Image src="" alt="sneakers" />
-        <div>
-          <Image src="" alt="sneakers" />
-          <Image src="" alt="sneakers" />
-          <Image src="" alt="sneakers" />
-          <Image src="" alt="sneakers" />
+      <section className="images">
+        <Image className="images_main" src="" alt="sneakers" />
+        <div className="previews">
+          <Image className="previews_item" src="" alt="sneakers" />
+          <Image className="previews_item" src="" alt="sneakers" />
+          <Image className="previews_item" src="" alt="sneakers" />
+          <Image className="previews_item" src="" alt="sneakers" />
         </div>
-      </div>
-      <div>
-        <h2>sneaker company</h2>
-        <h1>fall limited edition sneakers</h1>
-        <p>
+      </section>
+      <section className="info}">
+        <h2 className="collection">sneaker company</h2>
+        <h1 className="name">fall limited edition sneakers</h1>
+        <p className="description">
           these low-profile sneakers are your perfect casual wear companion.
           Featuring a durable rubber outer sole. They'll withstand everything
           the weather can offer.
         </p>
-        <div>
-          <span>$125.00</span>
-          <span>50%</span>
+        <div className="price">
+          <span className="price_value">$125.00</span>
+          <span className="price_discount">50%</span>
         </div>
-        <span>$250.00</span>
-        <div>
-          <div>
-            <span>-</span>
-            <span>0</span>
-            <span>+</span>
+        <span className="old_price">$250.00</span>
+        <div className="action">
+          <div className="amount">
+            <button className="amount_decrease">-</button>
+            <span className="amount_value">0</span>
+            <button className="amount_increase">+</button>
           </div>
-          <button>
-            <Image src="#" alt="" />
-            <span>Add to cart</span>
+          <button className="action_btn">
+            <Image className="btn_icon" src="#" alt="cart icon" />
+            <span className="btn_text">Add to cart</span>
           </button>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
